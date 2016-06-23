ebbfbe6a411105714c9f9b859fd2e7705bb6a06c
William Oberndorfer <will.oberndorfer@gmail.com>
2016-06-23 16:44:37 -0400

    update printing name example

--- Week1/02_printing.c
+++ Week1/02_printing.c
@@ -1,12 +1,20 @@
 /* FIU C Programming Summer Camp
  *
  * Example 2
- * Printing
+ * Printing a name
  */
 
 #include <stdio.h>
 
 int main(int argc, char *argv[])
 {
+    char name[80];
+    
+    printf("What is your name?\n");
+    
+    scanf("%s", name);
+    
+    printf("Hello, %s\n", name);
+    
     return 0;
 }
