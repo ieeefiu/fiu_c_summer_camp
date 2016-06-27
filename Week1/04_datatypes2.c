b14b9e218644532b605d633885108ad80743a027
William Oberndorfer <will.oberndorfer@gmail.com>
2016-06-27 17:33:11 -0400

    update datatypes example

--- Week1/04_datatypes2.c
+++ Week1/04_datatypes2.c
@@ -21,10 +21,27 @@ int main(void)
     result = number_1 * number_2;
     printf("%d times %d is %d\n", number_1, number_2, result);
 
-    /* What about division? */
+    /* What about division? Let's change number_1 to 12. */
+    number_1 = 12;
     result = number_1 / number_2;
     printf("%d divided by %d is %d\n", number_1, number_2, result);
-    printf("Something about that doesn't seem right...\n");
+
+    /* We can use the modulus operator % to get the remainder.
+     * Let's change number_1 to 13 so we get a remainder.
+     */
+    number_1 = 13;
+    result = number_1 % number_2;
+    printf("The remainder of %d divided by %d is %d\n",
+           number_1, number_2, result);
+
+    /* What happens when we divide a smaller number by a larger number? 
+     * Let's change number_1 to a smaller number and find out!    
+     */
+    number_1 = 2;
+    result = number_1 / number_2;
+    printf("%d divided by %d is %d\n"
+           "Something about that doesn't seem right...\n",
+           number_1, number_2, result);
     
     return 0;
 }
