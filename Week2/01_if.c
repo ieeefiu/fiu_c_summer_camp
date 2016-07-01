b41ce5c07d3900d5125e3db9306703d28f8dc3ca
William Oberndorfer <will.oberndorfer@gmail.com>
2016-07-01 11:32:50 -0400

    add if example

---
+++ Week2/01_if.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int number_example = 5;
+
+    if (number_example == 5) {
+        printf("Our number is 5\n");
+    }
+    else {
+        printf("Our number is not 5\n");
+    }
+
+    return 0;
+}
