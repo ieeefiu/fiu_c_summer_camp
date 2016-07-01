cde455e517a62f5366bdd6f3ca3c3c3869687c72
William Oberndorfer <will.oberndorfer@gmail.com>
2016-07-01 12:06:46 -0400

    add not equal example

---
+++ Week2/04_not_equal.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int number_1 = 3;
+
+    if (number_1 != 5) {
+        printf("Number does not equal 5\n");
+    }
+    else {
+        printf("Number equals 5\n");
+    }
+
+    return 0;
+}
