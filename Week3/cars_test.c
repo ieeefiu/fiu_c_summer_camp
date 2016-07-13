21e12a7c05a92016f57dc0d050fd48b44a619b5a
William Oberndorfer <will.oberndorfer@gmail.com>
2016-07-13 13:22:28 -0400

    add intro screen and display of winning car

--- Week3/cars_test.c
+++ Week3/cars_test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void print_car(int car, int position);
 
@@ -8,6 +9,16 @@ int main()
     
     int car_1_position = 1;
     int car_2_position = 1;
+
+    printf("\033[0;0H\033[2J");
+    
+    printf("Car Racing Game\n"
+           "Player 1 presses the 'a' key\n"
+           "Player 2 presses the 'l' key\n"
+           "When the referee says GO, press your key\n"
+           "To quit, enter 'q'\n");
+    print_car(1, car_1_position);
+    print_car(2, car_2_position);
     
     while (car_1_position < 60 && car_2_position < 60) {
 
@@ -22,6 +33,9 @@ int main()
         case 'l':
             car_2_position += 10;
             break;
+        case 'q':
+            printf("Thank you for playing!\n");
+            exit(0);
         default:
             break;
         }
@@ -42,6 +56,14 @@ int main()
             print_car(2, car_2_position);
         }
     }
+
+    if (car_1_position > car_2_position) {
+        printf("Car 1 is the winner!\n");
+    }
+    else {
+        printf("Car 2 is the winner!\n");
+    }
+    
     return 0;
 }
 
